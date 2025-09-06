dbe6f94a3d6856542582a45cc03846523589cd08
Simon Lay <ylay3633@uni.sydney.edu.au>
2025-09-07 09:08:04 +1000

    implementing testing

--- data_structure/trie/trie.py
+++ data_structure/trie/trie.py
@@ -1,10 +1,47 @@
 import string
 
 class Node: 
-    def __init__(self, value="a"): 
-        self.value = value
-        self.children = list(string.ascii_lowercase)
+    def __init__(self): 
+        self.children = [None] * 26
+        self.isEndOfWord = False
 
 class Trie:
-    def __init__(self, root):
-        self.root = root
+    def __init__(self): 
+        self.root = Node()
+
+    def insert(self, word): 
+        word = word.lower()
+        cursor = self.root
+        for letter in word: 
+            if cursor.children[ord(letter) - ord('a')] == None:
+                cursor.children[ord(letter) - ord('a')] = Node()
+            cursor = cursor.children[ord(letter) - ord('a')]
+        cursor.isEndOfWord = True
+        return self
+
+    def search(self, key):
+        return self.isMatch(key, self.root, 0, True)
+    
+    def startWith(self, prefix):
+        return self.isMatch(prefix, self.root, 0, False)
+        
+    
+    def isMatch(self, word, node, index, isFullMatch):
+        if node == None:
+            return False
+
+        if index == len(word):
+            return not isFullMatch or node.isEndOfWord
+        
+        return self.isMatch(word, node.children[ord(word[index]) - ord('a')], index+1, isFullMatch)
+        ...
+
+
+if __name__ == "__main__": 
+    trie = Trie()
+    trie.insert("cat").insert("discombobulate")
+    trie.insert("catan").insert("disconnect")
+    print(trie.search("cat"))
+    print(trie.search("dog"))
+    print(trie.startWith("ca"))
+    print(trie.startWith("dis"))
